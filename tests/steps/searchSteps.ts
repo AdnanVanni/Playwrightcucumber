e60ff82231cdbc036aaf9e880d38425485a27776
AdnanVanni <adnanvanni@gmail.com>
2024-10-28 18:52:54 +0530

    Update searchSteps.ts

--- tests/steps/searchSteps.ts
+++ tests/steps/searchSteps.ts
@@ -12,7 +12,7 @@ setDefaultTimeout(500 * 1000);
 Given('the application is loaded successfully', async function () {
   browser = await chromium.launch({ headless: false });
   page = await browser.newPage();
-  homepagenew = new SearchPage(page); // Initialize with the created page
+  homepagenew = new SearchPage(page); 
 
   await page.route('**/*', (route) => {
     if (route.request().resourceType() === 'image') {
@@ -26,7 +26,7 @@ Given('the application is loaded successfully', async function () {
 });
 
 When('user searches for word {string}', async function (this: CustomWorld, searchTerm: string) {
-  this.searchTerm = searchTerm; // Store searchTerm in CustomWorld
+  this.searchTerm = searchTerm; 
   await homepagenew.searchFor(this.searchTerm);
 });
 
@@ -36,7 +36,7 @@ When('user clicks on "Skill" Dropdown', async function () {
 
 When('user searches for "Automation testing" in Skill Dropdown', async function (this: CustomWorld,skill: string) {
   this.skill=skill;
-  await homepagenew.searchInSkillDropdown(this.skill); // Use skill from CustomWorld
+  await homepagenew.searchInSkillDropdown(this.skill); 
 });
 
 Then('user sees results matching the search term in the UI', async function () {
